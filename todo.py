d4e93a468cc26d6d364dc142ee52b4594483128a
MuadDAtr <int.stron@protonmail.com>
2022-08-15 23:56:12 +0200

    trying argparse

--- todo.py
+++ todo.py
@@ -1,12 +1,5 @@
 import argparse
-
 parser = argparse.ArgumentParser()
-parser.add_argument('-q', '--quiet',
-action ='store_true',
-dest = 'quiet',
-help = 'Suppress Output'
-)
-
+parser.add_argument("echo", help="echo the string you use here")
 args = parser.parse_args()
-
-print('Quiet mode is %r.' % args.quiet)
+print(args.echo)
