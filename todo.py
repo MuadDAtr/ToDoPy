44cb6f30a8c50baece257d768bcb1a759b1d07f4
MuadDAtr <int.stron@protonmail.com>
2022-08-16 00:00:03 +0200

    refactoring

--- todo.py
+++ todo.py
@@ -1,5 +1,16 @@
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo the string you use here")
-args = parser.parse_args()
-print(args.echo)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-q', '--quiet',
+                        action ='store_true',
+                        dest = 'quiet',
+                        help = 'Suppress Output'
+    )
+
+    args = parser.parse_args()
+
+    print('Quiet mode is %r.' % args.quiet)
+
+
