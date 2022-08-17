03bf472e6f39df882b91793eb967c601e4bc75cd
MuadDAtr <int.stron@protonmail.com>
2022-08-17 23:59:34 +0200

    trying argparse

--- todo.py
+++ todo.py
@@ -1,16 +1,7 @@
 import argparse
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-q', '--quiet',
-                        action ='store_true',
-                        dest = 'quiet',
-                        help = 'Suppress Output'
-    )
-
-    args = parser.parse_args()
-
-    print('Quiet mode is %r.' % args.quiet)
-
-
+parser = argparse.ArgumentParser()
+parser.add_argument("--verbose", help="increase output verbosity",
+                    action="store_true")
+args = parser.parse_args()
+if args.verbose:
+    print("verbosity turned on")
