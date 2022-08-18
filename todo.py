33919dfe018072dce82e4925d505c65aca8638b8
MuadDAtr <int.stron@protonmail.com>
2022-08-18 23:54:17 +0200

    continuing argparse

--- todo.py
+++ todo.py
@@ -4,12 +4,14 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', 
-                        help = 'Showing tasks to do', 
+                        help = 'Showing pending tasks', 
                         action= 'store_true'
     )
     parser.add_argument('-a',
                         help = 'Add new task'
     )
+    parser.add_argument('-t', 
+                        help = 'Tick the task (changing status)')
 
     args = parser.parse_args()
 
