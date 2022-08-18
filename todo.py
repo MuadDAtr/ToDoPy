a890073e0148b144e440a99b3fe5df0050a86cba
MuadDAtr <int.stron@protonmail.com>
2022-08-18 23:44:53 +0200

    continuing argparse

--- todo.py
+++ todo.py
@@ -3,11 +3,15 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('-l', 
+                        help = 'Showing tasks to do', 
+                        action= 'store_true'
+    )
     parser.add_argument('-a',
                         help = 'Add new task'
     )
 
     args = parser.parse_args()
 
-    print('Add mode is %r.' % args.add)
+    print('Add mode is %r.' % args.a)
 
