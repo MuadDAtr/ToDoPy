bce77092d877fbe58992768c0720a3af0ecd1fe0
MuadDAtr <int.stron@protonmail.com>
2022-08-19 00:04:11 +0200

    corrections

--- todo.py
+++ todo.py
@@ -3,19 +3,19 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', 
+    parser.add_argument('--l', 
                         help = 'Showing pending tasks', 
                         action= 'store_true'
     )
-    parser.add_argument('-a',
+    parser.add_argument('--a',
                         help = 'Add new task'
     )
-    parser.add_argument('-t', 
+    parser.add_argument('--t', 
                         help = 'Tick the task (changing status)')
 
     args = parser.parse_args()
 
-    print('Add mode is %r.', args)
+   
     if  args.a is not None:
         print('Adding...')
 
